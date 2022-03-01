862ac058c6f2211ba5983237e3f6b6421b0b03e0
5witchkr <hob4410@gmail.com>
2022-03-01 22:37:29 +0900

    content&kakaomap

--- newproject/settings.py
+++ newproject/settings.py
@@ -143,8 +143,8 @@ DATABASES['default'].update(db_from_env)
 # CORS 관련 추가
 CORS_ORIGIN_WHITELIST = ['https://projectsvtnew.herokuapp.com',
                          'http://projectsvtnew.herokuapp.com',
-                         # 'http://127.0.0.1:8080',
-                         # 'http://localhost:8080',
+                         'http://127.0.0.1:8080',
+                         'http://localhost:8080',
                          # 'http://127.0.0.1:8000',
                          # 'http://localhost:8000',
                          ]
